13254827d7e04b914af711a77938c98f8403d136
Shriyesh <shriyesh.chandra@gmail.com>
2024-11-17 01:56:00 -0500

    Writing to Smaller Data Files

--- clusters.py
+++ clusters.py
@@ -14,22 +14,22 @@ if __name__ == "__main__":
     parser = args_parser()
     parser.add_argument("-n", "--n-clusters", type=int, default=128)
     args = parser.parse_args()
-
+    print("About to Load Embeddings")
     embeddings = np.load(f"{args.data_dir}/embeddings.npy", mmap_mode='r')[:1000000]
-
+    print("Loaded Embeddings", embeddings.shape)
     kmeans = KMeans(n_clusters=args.n_clusters, init="k-means++", n_init=1, random_state=42, verbose=1, max_iter=5).fit(embeddings)
 
     cluster_centroids = kmeans.cluster_centers_
     print("cluster_centroids", cluster_centroids.shape)
-    np.save(f"{args.data_dir}/cluster_centroids.npy", cluster_centroids)
+    np.save(f"{args.data_dir}/cluster_centroids_Small_Data.npy", cluster_centroids)
 
     cluster2idx = [list() for _ in range(args.n_clusters)]
     for i, label in enumerate(kmeans.labels_):
         cluster2idx[label].append(i)
-    with open(f"{args.data_dir}/cluster_mappings.json", "w") as f:
+    with open(f"{args.data_dir}/cluster_mappings_Small_Data.json", "w") as f:
         json.dump(cluster2idx, f)
 
     for cluster, idxs in enumerate(cluster2idx):
         cluster_embeddings = embeddings[idxs]
-        print(f"cluster_embeddings_{cluster}", cluster_embeddings.shape)
-        np.save(f"{args.data_dir}/cluster_embeddings_{cluster}.npy", cluster_embeddings)
+        print(f"cluster_embeddings_{cluster}_Small_Data", cluster_embeddings.shape)
+        np.save(f"{args.data_dir}/cluster_embeddings_{cluster}_Small_Data.npy", cluster_embeddings)
